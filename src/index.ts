7ba26ca151ca19942f53e3af0b7b4341f40522d7
Donah Ongayo <donngeezzzzzzz@gmail.com>
2020-08-28 07:11:37 +0800

    refactor(server): added apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
+/* eslint-disable no-empty */
 import Koa from 'koa';
-import graphqlHTTP from 'koa-graphql';
-import mount from 'koa-mount';
-import schema from './graphql/schema';
-import root from './graphql/root';
+//import graphqlHTTP from 'koa-graphql';
+//import mount from 'koa-mount';
 import mongoose from 'mongoose';
 import bodyParser from 'koa-bodyparser';
+import koaRouter from 'koa-router';
+import { ApolloServer } from 'apollo-server-koa';
+import auth from './middleware/auth';
+
+import * as graphlSchema from './graphql';
+
+const server = new ApolloServer(graphlSchema);
 
 const app = new Koa();
-app.use(bodyParser());
+const router = new koaRouter();
 
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(bodyParser());
 mongoose.connect(`mongodb://127.0.0.1:27017/docker-mongo`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,15 +27,16 @@ mongoose.connect(`mongodb://127.0.0.1:27017/docker-mongo`, {
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('🚀 Database connected.'));
+db.once('open', () => console.log('Database connected.'));
 
-app.listen(9000);
-
+app.listen(9000, () => console.log('🚀 Server ready.'));
 app.on('error', (err) => {
   console.log('Server error', err);
 });
 
-app.use(
+server.applyMiddleware({ app });
+
+/*app.use(
   mount(
     '/graphql',
     graphqlHTTP({
@@ -34,6 +45,6 @@ app.use(
       graphiql: true,
     }),
   ),
-);
+);*/
 
 //export default server;
